818f985a8e14d7e776547dd3ec4031588df732a2
Mark Castle <mark@castle.sx>
2016-04-07 21:34:14 -0700

    Implement New Note Functionality

--- client/www/app/notes/notes.controller.js
+++ client/www/app/notes/notes.controller.js
@@ -5,9 +5,40 @@
     .module('notesee.notes')
     .controller('NotesController', NotesController)
 
-  function NotesController (Notes) {
+  function NotesController ($cordovaCamera, $ionicModal, $scope, Notes) {
     var vm = this
 
     vm.notes = Notes
+
+    vm.create = create
+    vm.save = save
+    vm.selectPhoto = selectPhoto
+    vm.takePhoto = takePhoto
+
+    function create () {
+      $ionicModal.fromTemplateUrl('app/notes/new.html', {scope: $scope})
+        .then(function (modal) {
+          vm.modal = modal
+          vm.modal.show()
+        })
+    }
+
+    function save () {
+      vm.modal.remove()
+    }
+
+    function selectPhoto () {
+      $cordovaCamera.getPicture({destinationType: Camera.DestinationType.DATA_URL, sourceType: Camera.PictureSourceType.PHOTOLIBRARY})
+        .then(function (image) {
+          vm.newPhoto = image
+        })
+    }
+
+    function takePhoto () {
+      $cordovaCamera.getPicture({destinationType: Camera.DestinationType.DATA_URL})
+        .then(function (image) {
+          vm.newPhoto = image
+        })
+    }
   }
 })()
