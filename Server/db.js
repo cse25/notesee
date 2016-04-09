4e36789fc5e9fb14e4931cd79a46836c44622a20
Danny Thai <dthai92@yahoo.com>
2016-04-08 22:48:07 -0700

    [bug] Database connection does not time out

--- Server/db.js
+++ Server/db.js
@@ -31,7 +31,7 @@ fs.readFile(__dirname + '/schema.sql', 'utf-8', function(err, data){
   })
 })
 
-// setInterval(function () { db.query('SELECT 1') }, 5000)
+setInterval(function () { db.query('SELECT 1') }, 5000)
 
 module.exports = db
 
