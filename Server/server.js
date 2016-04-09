f483b4315a50738ba438a763a136e7c7f7899f86
Danny Thai <dthai92@yahoo.com>
2016-04-08 21:35:36 -0700

    [refactor] modularized routes

--- Server/server.js
+++ Server/server.js
@@ -3,25 +3,24 @@ var app = express()
 var cors = require('cors')
 var bodyParser = require("body-parser")
 var db = require('./db.js')
-var router = require('express').Router()
+
+// your routes requires go here
+var users = require('./Routes/users')
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-app.use('/api', router)
+
+// declare path here 
+app.use('/api/users/', users)
+
+
 
 var port = process.env.PORT || 8080
 
 
-router.get('/', function (req, res){
-  res.json({ message: 'Notesee API working' })
-})
 
-// login
-router.get('/users/:id', function (req, res) {})
 
-// signup/create a user
-router.post('/users', function (req, res) {})
 
 
 
@@ -33,7 +32,7 @@ app.listen(port, console.log('Magic happens on port', port))
 // classrooms/:id -> return classroom information (code, metadata, created_at, etcetera)
 // classrooms/:id/notes -> return all notes in that classroom
 // classrooms/:id/users -> return all users in that classroom
-// users -> return all users
+// users/ -> return all users and their data
 // users/:id -> return user information (name, data joined, etcetera)
 // users/:id/classrooms -> return all classrooms user is in
 // users/:id/saved -> return all saved notes user has
