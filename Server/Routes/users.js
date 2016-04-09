625563717e195e847e274a6ed99a94b9734b9b02
Danny Thai <dthai92@yahoo.com>
2016-04-08 21:36:11 -0700

    [feat] create user routes

---
+++ Server/Routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db.js')
+
+router.get('/', function(req, res) {
+	// send back all registered users
+	
+})
+
+
+
+
+
+
+
+
+module.exports = router;
