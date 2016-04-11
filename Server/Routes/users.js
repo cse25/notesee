6c38863fb72e63088410041dc06fa2e2cc5e5b11
Danny Thai <dthai92@yahoo.com>
2016-04-10 19:21:19 -0700

    [feat] Add user route: grab user information

--- Server/Routes/users.js
+++ Server/Routes/users.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var db = require('../db.js')
 
-router.get('/', function(req, res) {
-	// send back all registered users
-	res.JSON({name: 'Bob'});
+
+// users/ -> return all users and their data
+// users/:id -> return user information (name, data joined, etcetera)
+// users/:id/classrooms -> return all classrooms user is in
+// users/:id/saved -> return all saved notes user has
+
+
+// grab user information
+router.get('/id:', function(req, res) {
+	var user = req.user.id
+	db.query('SELECT `full_name`, `username`, `createdAt` FROM `USERS` WHERE `id` = ?;', [user], function (err, rows) {
+		if (err) {
+			console.error(err)
+			res.sendStatus(500)
+		} else {
+			res.json(rows)
+		}
+	})
 })
 
 
@@ -13,5 +28,4 @@ router.get('/', function(req, res) {
 
 
 
-
-module.exports = router;
+module.exports = router
