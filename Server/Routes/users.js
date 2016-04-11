3419c1330798bc438de2a3816fde58b25bd7d834
Danny Thai <dthai92@yahoo.com>
2016-04-11 10:47:18 -0700

    [refactor] Can obtain user info from specified user

--- Server/Routes/users.js
+++ Server/Routes/users.js
@@ -9,13 +9,13 @@ var url = require('url')
 // users/:id/saved -> return all saved notes user has
 
 
-// grab user information
-router.get('/user/kan', function (req, res) {
-	var path = req.route.path;
-	var pathArr = path.split('/');
-	var user = pathArr[2];
-	console.log('pathArr: ', pathArr);
-	db.query('SELECT `full_name`, `username`, `createdAt` FROM `USERS` WHERE `username` = ?;', [user], function (err, rows) {
+// grab user information by specifying username
+// ex. /users/user/danny => grab all user information for Danny from users table
+router.get('/user/:name', function (req, res) {
+	var user = req.params.name;
+	db.query('SELECT `full_name`, `username`, `createdAt` FROM `USERS` WHERE `username` = ?;',
+	 [user], 
+	 function (err, rows) {
 		if (err) {
 			console.error(err)
 			res.sendStatus(500)
