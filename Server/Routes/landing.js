ac7c0844fcd93bdc0fc0e1092321a55b07d69ba9
Danny Thai <dthai92@yahoo.com>
2016-04-11 09:39:36 -0700

    [feat] Add user route: register user

---
+++ Server/Routes/landing.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var router = express.Router()
+var db = require('../db.js')
+
+// register user
+router.post('/register', function (req, res) {
+	var username = req.body.user.username
+	var password = req.body.user.password
+	db.query('INSERT INTO `USERS` SET `username` = ?, `password` = ?;',
+		[username, password],
+		function (err, rows) {
+			if (err) {
+				console.error(err)
+				res.status(500)
+			} else {
+				res.status(201).json({success: true});
+			}
+		})
+})
+
+module.exports = router
